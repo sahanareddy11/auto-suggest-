8b046d2d621c45fdfce1f8a1bdeeb1697d3839a8
Sahana Reddy <24wh1a05l4@bvrithyderabad.edu.in>
2026-07-03 13:37:58 +0530

    second commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 3002;
+
+//web server :
+app.use(express.static("frontend"))
+
+app.listen(PORT, function(){
+    console.log("successfully running at http://localhost:"+PORT)
+})
